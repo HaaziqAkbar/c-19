8159ac0f489673753cb99a50e42016cde9307b0b
HaaziqAkbar <99976687+HaaziqAkbar@users.noreply.github.com>
2022-05-11 20:35:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,124 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play";
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+}
+
+function setup() {
+  createCanvas(600, 600);
+
+  spookySound.loop();
+
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+
+  doorsGroup = new Group ();
+  climbersGroup = new Group ();
+  invisibleBlockGroup = new Group();
+
+  ghost = createSprite(200,100,5,5);
+  ghost.scale = 0.4;
+  ghost.addImage("ghost",ghostImg);
+ 
+  
+
+}
+
+function draw() {
+ 
+ 
+ if(gameState === "play") {
+
+  background(200);
+
+  if(tower.y > 400){
+      tower.y = 300
+    }
+   
+  if(keyDown("right_arrow")){
+  ghost.x  = ghost.x +5;
+
+  }
+  
+  if(keyDown("left_arrow")){
+    ghost.x  = ghost.x -5;
+  
+    }
+  
+  if (keyDown("space")) {
+    ghost.velocityY = -2;
+  } 
+
+  ghost.velocityY = ghost.velocityY +0.5;      
+
+  if (climbersGroup.isTouching(ghost)){
+    ghost.velocityY = 0;                                                         
+  }
+
+  if(invisibleBlockGroup.isTouching(ghost) || ghost.y > 600){
+    ghost.destroy();
+    gameState = "end";
+
+  }
+
+  spawndoor ();
+  drawSprites();
+}
+
+
+
+if(gameState === "end"){
+  background ("blue")
+  stroke("yellow");
+  fill ("yellow");
+  textSize (25);
+  text("Game Over",250,250);
+
+}
+  
+   
+}
+
+function spawndoor () {
+   
+  if (frameCount % 240 === 0) { 
+   
+   var door = createSprite (200,-50);
+   door.addImage("door",doorImg);
+   door.x = Math.round(random(120,400));
+   door.velocityY = 1;
+   door.lifetime = 800;
+   doorsGroup.add(door);
+
+   var climber = createSprite(200,10);
+   climber.addImage("climber",climberImg);
+   climber.x = door.x;
+   climber.velocityY = door.velocityY;
+   climber.lifetime = 800;
+   climbersGroup.add(climber);
+
+   invisibleBlock = createSprite(200,13);
+   invisibleBlock.width = climber.width;
+   invisibleBlock.height = 2;
+   invisibleBlock.x = door.x;
+   invisibleBlock.velocityY = 1;
+   invisibleBlock.debug = true;
+   invisibleBlockGroup.add(invisibleBlock);
+
+
+   ghost.depth = door.depth;
+   ghost.depth = ghost.depth +1;
+                    
+}
+
+}
